d307378f348f6eb81c7455eb57859406477f26e3
Arayaphong Traisopon <arayaphongt@wha-digital.com>
2025-06-23 09:49:21 +0700

    Remove KingPiece class and related files to simplify piece management

--- src/KingPiece.cpp
+++
@@ -1,8 +0,0 @@
-#include "KingPiece.h"
-
-KingPiece::KingPiece() : Piece() {}
-
-KingPiece::KingPiece(const std::string& color, Position position)
-    : Piece(color, position) {}
-
-// Implement king-specific methods here if needed
